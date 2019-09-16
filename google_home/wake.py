c341c82f6ced07542d8f327c66b6d6856400c40b
satsukiya <kai.walker.0329@gmail.com>
2019-09-16 16:59:28 +0900

    get dynamic local ip address

--- google_home/wake.py
+++ google_home/wake.py
@@ -1,11 +1,17 @@
 import pychromecast
 import sys
+import netifaces as ni
 
-def main(http_link):
+def localaddress():
+	return ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr']
+
+
+def main(music_title):
 	chromecasts = pychromecast.get_chromecasts()
 	google_home = chromecasts[0]
 
 	google_home.wait()
+	http_link = "http://{0}/{1}".format(localaddress(), music_title)
 	mc = google_home.media_controller
 	mc.play_media(http_link,'audio/mp3')
 	mc.block_until_active()
@@ -16,4 +22,3 @@ if __name__ == '__main__':
 		main(args[1])
 	else :
 		print("[usage] you should command a link.")
-	
